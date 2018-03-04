f2f1f3e491706095e4307d66ff49cfd21c02d5d5
Hung-Su Nguyen <hungsu.nguyen@gmail.com>
2018-03-04 04:02:58 -0800

    feat: Add tested function to get hero name from hero ID

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,5 @@
 let calculateWinrateImprovement = require('../js/calculateWinrate.js')
+const getHeroName = require('../js/getHeroName')
 
 // tests
 console.assert(
@@ -9,3 +10,13 @@ console.assert(
 	calculateWinrateImprovement(10, 10, 15, 20) == -0.25,
 	'Playing 50% of games against sniper, where I always win against sniper, then banning sniper, should worsen winrate'
 )
+console.log('All calculateWinrate.js tests passed')
+
+console.assert(getHeroName(1) == 'Anti-Mage', 'Hero ID 1 should be "Anti-Mage"')
+console.assert(getHeroName("1") == '', 'Hero ID 1 should be ""')
+console.assert(getHeroName(24) == '', 'Hero ID 24 should be ""')
+console.assert(getHeroName(31) == 'Lich', 'Hero ID 1 should be "Anti-Mage"')
+console.assert(getHeroName(120) == 'Pangolier', 'Hero ID 120 should be "Pangolier"')
+console.log('All getHeroName.js tests passed')
+
+console.log('All tests passed')
